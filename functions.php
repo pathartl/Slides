7743e2c809ba7da021c012de9638ba3559e57fad
Pat Hartl <pat@nsrit.com>
2014-07-01 10:53:52 -0500

    Added 900x450 thumbnail size for header images.

--- functions.php
+++ functions.php
@@ -26,7 +26,8 @@ if (function_exists('add_theme_support'))
     add_theme_support('menus');
 
     // Add Thumbnail Theme Support
-    add_theme_support('post-thumbnails');
+    add_theme_support('post-thumbnails'); // Featured thumbnail
+    set_post_thumbnail_size( 900, 450, true );
     add_image_size('large', 700, '', true); // Large Thumbnail
     add_image_size('medium', 250, '', true); // Medium Thumbnail
     add_image_size('small', 120, '', true); // Small Thumbnail
